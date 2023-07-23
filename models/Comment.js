b615ff7e22a46646aceb6bc025efa8122cb72573
JoelP-11 <Jp421513@gmail.com>
2023-07-22 20:40:54 -0700

    models is done

---
+++ models/Comment.js
@@ -0,0 +1,20 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+// is model wrong?
+
+class Comment extends Model { }
+
+Comment.init(
+    {
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize
+    }
+);
+
+module.exports = Comment;
