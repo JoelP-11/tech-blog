7d14bfccc1d0b93d9a738cfd3157994659775a96
JoelP-11 <Jp421513@gmail.com>
2023-07-23 15:55:52 -0700

    got the server to at least run but now the web app isnt really loading much

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
@@ -7,7 +7,6 @@ const sequelize = process.env.JAWSDB_URL
 : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3001
 });
 
 module.exports = sequelize;
--- db/schema.sql
+++ db/schema.sql
@@ -2,4 +2,4 @@ DROP DATABASE IF EXISTS tech_blog_db;
 
 CREATE DATABASE tech_blog_db;
 
-GRANT ALL PRIVILEGES ON tech_blog_db TO 'joel11'@'localhost' IDENTIFIED BY 'root';
+
