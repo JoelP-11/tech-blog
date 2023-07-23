2efd6632cbbfe85b5dc4688706c31d72f119f32c
JoelP-11 <Jp421513@gmail.com>
2023-07-23 16:31:05 -0700

    app is done, might do styling changes

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlerbars', hbs.engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
